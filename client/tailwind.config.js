bb358913d47276f90c191b6de18519dc053d2da3
Sajal Mittal <sajalmittal@Sajals-MacBook-Air.local>
2024-04-25 22:26:30 +0530

    revamped project

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // navy: '#152C5B',
+        // sky: '#138EFF',
+        navy:'#435334',
+        sky:'#7A9D54',
+        back: "#F6FBF4" 
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
